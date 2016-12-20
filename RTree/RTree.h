d1603afd794de33f56bb8ba8ca904c259a3ef306
Elias <elpumitaelias@gmail.com>
2016-12-20 16:36:02 -0500

    Templates and insert function are callable! Still not functioning :(

--- RTree/RTree.h
+++ RTree/RTree.h
@@ -7,21 +7,37 @@ using namespace std;
 #define RTree_temp template<typename Data_t, typename Elem_t, size_t NDims, int NMaxNodes, int NMinNodes>
 #define RTree_t RTree<Data_t, Elem_t, NDims, NMaxNodes, NMinNodes>
 
-template <typename Data_t, typename Elem_t, size_t NDims = 2, int NMaxNodes = 3, int NMinNodes = NMaxNodes/2>
+template <typename Data_t, typename Elem_t, size_t NDims = 2, int NMaxNodes = 8, int NMinNodes = NMaxNodes/2>
 class RTree
 {
 protected:
     struct Page;
 
 public:
+    struct Point;
+
     RTree();
     virtual ~RTree();
 
-    void insert();
+    // void insert()
+    void insert(Point point, Data_t data) 
+    {
+        cout << data << endl;
+    }
 
+    struct Point {
+        Point()                                     {}
+        Point(Elem_t x)                             {   axes[0] = x; }
+        Point(Elem_t x, Elem_t y)                   {   axes[0] = x; axes[1] = y;    }
+        Point(Elem_t x, Elem_t y, Elem_t z)         {   axes[0] = x; axes[1] = y; axes[2] = z;    }
+
+        Elem_t  axes[NDims];
+    };
+    
+protected:
     struct Box {
-        Elem_t  min[NDims],
-                max[NDims];
+        Point   min,
+                max;
     };
 
     struct Node {
@@ -40,17 +56,21 @@ public:
     };
 
 private:
-    
+    Page *root;
 };
 
 RTree_temp
 RTree_t::RTree()
 {
+    root = new Page;
+    root->level = 0;        //Es hoja cuando se crea
 }
 
 RTree_temp
 RTree_t::~RTree()
 {
+    if(root)
+        delete root;
 }
 
 #undef RTREE_TEMPLATE
--- RTree/main.cpp
+++ RTree/main.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
+typedef RTree<char[], int, 2, 3, 1> MyRTree;
+
 int main() {
     cout << "Hello World!" << endl;
+
+    MyRTree my_tree;
+    my_tree.insert(MyRTree::Point(5, 45), "Denver");
+    my_tree.insert(MyRTree::Point(90,  5), "Miami");
+    my_tree.insert(MyRTree::Point(85, 15), "Atlanta");
+    my_tree.insert(MyRTree::Point(52, 10), "Mobile");
+    my_tree.insert(MyRTree::Point(82, 65), "Buffalo");
+    my_tree.insert(MyRTree::Point(62, 77), "Toronto");
+    my_tree.insert(MyRTree::Point(32, 42), "Chicago");
+    my_tree.insert(MyRTree::Point(27, 35), "Omaha");
+
     return 0;
 }
