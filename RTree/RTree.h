d3892e5848fb972f4c54dc1fe86a43a630b955ad
Elias Puma <elpumitaelias@gmail.com>
2016-12-20 09:34:08 -0500

    First commit of struct header

---
+++ RTree/RTree.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class RNode
+{
+public:
+    RNode();
+    ~RNode();
+
+private:
+    
+};
+
+RNode::RNode()
+{
+}
+
+RNode::~RNode()
+{
+}
+
+class RTree
+{
+public:
+    RTree();
+    ~RTree();
+
+private:
+    
+};
+
+RTree::RTree()
+{
+}
+
+RTree::~RTree()
+{
+}
